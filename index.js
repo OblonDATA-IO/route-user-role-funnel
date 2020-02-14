4ae514df902c1b51dde784759ce92bebee3ff409
UnKnoWn <thomas123456789000@gmail.com>
2020-02-15 05:25:16 +0800

    [MAJOR] Refactored middleware to work with accepted values in array
    Signed-off-by: UnKnoWn <thomas123456789000@gmail.com>

--- index.js
+++ index.js
@@ -9,22 +9,26 @@
  * @function
  * @param {string} path - field that is to be matched in the request object
  * @param {string} field - field that is to be matched in the request object
- * @param {string} role - User role that is matched
+ * @param {string, array} roles - User role that is matched
  * @return {function}
  */
-module.exports = (path = "user", field, role) => {
+module.exports = (path = "user", field, roles) => {
     if (!field) {
         throw new Error("Field has to be a string");
     }
 
-    if (!role) {
-        throw new Error("User role has to be a string");
+    if (!roles) {
+        throw new Error("Roles has to be a string or an array");
+    }
+
+    if (typeof roles === "string") {
+        roles = [roles];
     }
 
     return (req, res, next) => {
         if (
-            req.user &&
-            req[path][field] === role
+            req[path] &&
+            roles.includes(req[path][field])
         ) {
             next();
         } else {
